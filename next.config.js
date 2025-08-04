f8f6270ea655a5c8a69a95e1759ab6377dc3586b
Hlyda <selablindahind@gmail.com>
2025-08-04 23:23:27 +0200

    fix next config js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
